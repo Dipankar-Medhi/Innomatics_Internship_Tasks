30cc8eab361cd2939b562ce71ccc9d7c5c944be6
Dipankar-Medhi <dipumedhi99@gmail.com>
2021-10-24 17:38:14 +0530

    update

--- Task_5(for advanced python programmers)/Subsets.py
+++ Task_5(for advanced python programmers)/Subsets.py
@@ -6,42 +6,16 @@ Input: nums = [1,2,3]
 Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]"""
 
 
-# class Solution:
-#     def subsets(self, nums):
-#         result = [[]]
-
-#         for n in nums:
-#             for i in range(len(result)):
-
-#                 # Since [] + [1] = [1], [1] + [2] = [1,2], and so on...
-#                 result.append(result[i]+[n])
-#         return result
-
 class Solution:
     def subsets(self, nums):
-        # for example nums is [5, 6, 1], input number is 3, convert to binary is 011, then return list should be [6, 1]
-        # if input number is 5 which binary is 101, then return list is [5, 1]
-        def bit2list(n):
-            ret = []
-            idx = 0
-            while n:
-                if n & 1:
-                    ret.append(nums[idx])
-                n >>= 1
-                idx += 1
-            return ret
-        # convert nums to binary, if len(nums)=3, output is 111; if len(nums)=4, output is 1111
-        base, one = 0, 1
-        for _ in range(len(nums)):
-            base += one
-            one <<= 1
-        # Use bit manipulation to find subsets; e.g. base=11, subsets: 10, 01
-        ret = []
-        mask = base
-        while mask:
-            ret.append(bit2list(mask))
-            mask = (mask - 1) & base
-        return ret + [[]]
+        result = [[]]
+
+        for n in nums:
+            for i in range(len(result)):
+
+                # Since [] + [1] = [1], [1] + [2] = [1,2], and so on...
+                result.append(result[i]+[n])
+        return result
 
 
 print(Solution().subsets([1, 2, 5]))
